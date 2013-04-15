f4cfbfdda24396af2b55225a9d5be3d790124c19
Chris Rasys <chris.rasys@gmail.com>
2013-04-15 02:38:52 -0400

    Basic class for characters

---
+++ classes/character.php
@@ -0,0 +1,17 @@
+<?php
+
+class character {
+
+    public $stats;
+    public $ability_groups;
+
+    public function __construct() {
+        $this->stats = new primary_stats();
+
+        $this->stats->Agility = 7;
+
+        $this->ability_groups = new ability_groups();
+
+        $this->ability_groups->calculate($this->stats);
+    }
+}
