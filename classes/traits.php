50bc7f1e5c2b7377c9fbcbb4050b36d4c00f12d6
Chris Rasys <chris.rasys@gmail.com>
2013-04-15 01:09:51 -0400

    Initial file for creating some basic class traits

---
+++ classes/traits.php
@@ -0,0 +1,18 @@
+<?php
+
+trait BasicConstruct {
+
+    private $allowed_construct_vars = array();
+
+    abstract public function setup();
+
+    public function __construct($value_array) {
+        $this->setup();
+
+        foreach ($value_array as $name => $value) {
+            if (property_exists($this, $name) && in_array($name, $this->allowed_construct_vars)) {
+                $this->$name = $value;
+            }
+        }
+    }
+}
