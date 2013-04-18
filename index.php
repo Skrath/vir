c43f7517bec6682ed7142b71d1bd5fb76c67c8dd
Chris Rasys <chris.rasys@gmail.com>
2013-04-18 01:54:34 -0400

    Add a few more includes, path constants

--- index.php
+++ index.php
@@ -2,15 +2,19 @@
 
 require('classes/traits.php');
 require('classes/stats.php');
+require('classes/abilities.php');
+require('classes/character.php');
+
+define('BASE_DIR', __DIR__);
+define('XML_DIR', BASE_DIR . '/xml');
 
 
-$stats = new primary_stats();
 
-$stats->strength->value = 8;
+$character = new character();
 
-var_dump($stats->strength->value);
+/* $character->ability_groups->loadFromXML(); */
 
-var_dump($stats);
+var_dump($character->ability_groups);
 
 //echo $stats->strength;
 
