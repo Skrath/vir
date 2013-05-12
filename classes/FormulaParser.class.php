7ea71d4dd183e7790fc5f9a34652ada924e9e68f
Chris Rasys <chris.rasys@gmail.com>
2013-05-12 14:21:19 -0400

    Parsing functionality for basic math and data lookups

--- classes/FormulaParser.class.php
+++ classes/FormulaParser.class.php
@@ -3,10 +3,13 @@ namespace vir;
 
 class FormulaParser {
 
+    private $character;
     private $formulas = array();
     private $currentObject;
 
-    public function __construct() {
+    public function __construct($character) {
+        $this->character = $character;
+
         $file = XML_DIR . '/formulas.xml';
 
         if (file_exists($file)) {
@@ -23,34 +26,93 @@ class FormulaParser {
 
         if (array_key_exists($formula, $this->formulas)) {
             $this->currentObject = $object;
-            $this->parse($this->formulas[$formula]);
+            $xml = $this->formulas[$formula];
+            foreach ($xml->children() as $child_xml) {
+                $this->parse($child_xml);
+            }
         }
 
     }
 
     private function parse($xml) {
+        $command = $xml->getName();
 
-        foreach ($xml->children() as $command => $xml) {
-            if (method_exists($this, $command)) {
-                $this->$command($xml);
-            }
-        }
+        if ( method_exists($this, $command) && ($xml->count() > 0) ) {
+            return $this->$command($xml);
+        } else return (string)$xml;
     }
 
     private function set($xml) {
 
         $var = (string)$xml->attributes()['name'];
 
-        if (is_null($xml->attributes()['value'])) {
-
-        } else {
-            $value = $xml->attributes()['value'];
-        }
+        $value = $this->parse($xml->children());
 
         $cast_type = (is_null($xml->attributes()['cast'])) ? 'string' : $xml->attributes()['cast'];
 
         settype($value, $cast_type);
 
+        //todo: actually set new value here
+    }
+
+    private function add($xml) {
+        $total = 0;
+
+        foreach ($xml->children() as $element) {
+            $total += (integer)$this->parse($element);
+        }
+
+        return $total;
+    }
+
+    private function subtract($xml) {
+        $total = 0;
+
+        foreach ($xml->children() as $element) {
+            $total -= (integer)$this->parse($element);
+        }
+
+        return $total;
+    }
+
+    private function multiply($xml) {
+        $total = 1;
+
+        foreach ($xml->children() as $element) {
+            $total *= (integer)$this->parse($element);
+        }
+
+        return $total;
+    }
+
+    private function data($xml) {
+        $type = $xml->attributes()['type'];
+
+        $value = ( count(get_object_vars($xml->children())) > 0 ) ? $this->parse($xml->children()) : (string)$xml->data;
+
+        switch ($type) {
+            case 'simple':
+                $value = $xml;
+                break;
+
+            case 'primary_stat':
+                $value = $this->getPrimaryStat($value);
+                break;
+
+            default:
+        }
+
+        return $value;
+    }
+
+    private function getPrimaryStat($stat) {
+        return $this->character->primary_stats->$stat->value;
+    }
+
+    private function objRef($xml) {
+        $var = (string)$xml->attributes()['name'];
+
+        return $this->currentObject->$var;
     }
 
 }
